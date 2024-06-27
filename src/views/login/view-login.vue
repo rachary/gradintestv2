e03d0970bb0ce522d2b2e8434a9bb40470d41cd8
rachary <rachmad.ary1@gmail.com>
2024-06-27 18:53:48 +0700

    update login

--- src/views/login/view-login.vue
+++ src/views/login/view-login.vue
@@ -16,7 +16,7 @@ console.log(isAuth.value)
 <template>
   <main>
     <div class="flex w-full h-screen">
-      <div class="w-full sm:w-1/2 bg-primary-1 h-full flex items-center justify-center">
+      <div class="w-full md:w-1/2 bg-primary-1 h-full flex items-center justify-center">
         <h1 class="font-lato text-secondary-1 py-2 px-4 text-5xl inset-0 absolute flex">gradin <span><vue-writer class="text-primary-2" :array="['test', 'chat']"></vue-writer></span></h1>
         <div class="card w-96 bg-secondary-1 ">
           <div class="p-8 space-y-4">
@@ -27,8 +27,8 @@ console.log(isAuth.value)
           </div>
         </div>
       </div>
-      <div class="w-1/2 h-full bg-secondary-1 flex items-center justify-center">
-        <div class="w-1/2 relative hidden xl:block">
+      <div class="w-1/2 h-full bg-secondary-1 items-center justify-center hidden md:flex">
+        <div class="w-1/2 relative hidden xl:flex">
           <img src="/images/laptop.png" alt="" class="w-full">
           <img src="/images/1.png" alt="" class="absolute top-4  right-[60px] w-[75%] object-fill h-[62%]">
           <img src="/images/2.png" alt="" class="absolute top-4  right-[60px] w-[75%] object-fill h-[62%]">
