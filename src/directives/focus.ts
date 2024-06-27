8a32c0b995aa7954f0cc1f7be75226ab34a41e60
rachary <rachmad.ary1@gmail.com>
2024-06-27 16:35:28 +0700

    add focus directives

---
+++ src/directives/focus.ts
@@ -0,0 +1,33 @@
+import type { App, DirectiveBinding } from "vue";
+
+export default {
+	install(app: App) {
+		app.directive('focus', {
+      mounted(el: HTMLElement) {
+        const focusElement = () => {
+          if (el instanceof HTMLDivElement) {
+            const component = el.querySelector('input, textarea');
+            if (component instanceof HTMLInputElement || component instanceof HTMLTextAreaElement) {
+              component.focus();
+            }
+          } else {
+            el.focus();
+          }
+        };
+        focusElement();
+      },
+      updated(el: HTMLElement, binding: DirectiveBinding) {
+        if (binding.value && !binding.oldValue) {
+          if (el instanceof HTMLDivElement) {
+            const component = el.querySelector('input, textarea');
+            if (component instanceof HTMLInputElement || component instanceof HTMLTextAreaElement) {
+              component.focus();
+            }
+          } else {
+            el.focus();
+          }
+        }
+      }
+		});
+	},
+};
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import focus from '@/directives/focus'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(focus)
 app.use(createPinia())
 app.use(router)
 
