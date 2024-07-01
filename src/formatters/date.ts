1b14cd4599f66d640c58ea1afe84ebdf27485f48
rachary <rachmad.ary1@gmail.com>
2024-07-01 17:35:22 +0700

    update formatted time

--- src/formatters/date.ts
+++ src/formatters/date.ts
@@ -1,19 +1,17 @@
-import { format, isToday, differenceInSeconds } from 'date-fns'
+import { format, isToday, differenceInSeconds, differenceInMinutes } from 'date-fns'
 
-export function formatTime(date: Date): string {
-  const now = new Date()
-
-  if (differenceInSeconds(now, date) < 60) {
-    return 'just now'
-  }
-
-  if (isToday(date)) {
-    return format(date, 'HH:mm')
-  }
-  
-  if (date == null) {
+export function formatTime(date: Date|string) {
+  if (!date) {
     return ' '
   }
 
+  const now = new Date()
+  
+  if (differenceInSeconds(now, date) < 60) {
+    return 'just now'
+  }
+  if (isToday(date)) {
+    return format(date, 'HH:mm')
+  }
   return format(date, 'dd/MM/yy')
 }
